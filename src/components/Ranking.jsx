37961961bc4b70b43b69bb9e2953e90230d3e03d
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-06-18 18:03:57 +0900

    랭킹 데이터 가져올동안 보여줄 텍스트 컴포넌트 추가

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
 import "./Ranking.css";
 
 const sortArr = (arr) => {
-  console.log("sortArr !!!");
+  // console.log("sortArr !!!");
   return arr.sort((a, c) => c.score - a.score);
 };
 
 const Ranking = ({ updateRanking }) => {
-  console.log("Ranking 컴포넌트!!!");
+  // console.log("Ranking 컴포넌트!!!");
 
   const [scores, setScores] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getScores = async () => {
-    console.log("getScores!!!");
+    // console.log("getScores!!!");
 
     try {
       // const res = await fetch("/api/scores");
@@ -20,6 +22,7 @@ const Ranking = ({ updateRanking }) => {
         "https://show-me-typing.herokuapp.com/api/scores"
       );
       const data = await res.json();
+      setIsLoading(false);
       setScores(sortArr(data));
     } catch (error) {
       console.log(error);
@@ -34,6 +37,7 @@ const Ranking = ({ updateRanking }) => {
     <div className="ranking-container">
       <div className="outline">
         <div className="ranking-title">RANKING</div>
+        {isLoading && <Loading />}
         <ul className="ranking-list">
           {scores.map(({ score, username, _id }) => {
             return (
